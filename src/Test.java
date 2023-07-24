798a1ab804a70843c0d63956db5b3c2d5316c8be
cyp <cyp@123.com>
2023-07-24 23:04:38 +0800

    第一次提交

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Description:
+ * @Author: PingZi
+ * @project:Work
+ * @DateTime: 2023/7/24 23:03
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("h");
+    }
+}
